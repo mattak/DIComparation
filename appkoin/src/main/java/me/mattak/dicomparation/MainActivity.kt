8cfef46d354a97981216c478e57568b819151b5e
mattak <mattak.me@gmail.com>
2023-02-11 19:17:10 +0900

    :seedling: main add multidex support

--- appkoin/src/main/java/me/mattak/dicomparation/MainActivity.kt
+++ appkoin/src/main/java/me/mattak/dicomparation/MainActivity.kt
@@ -31,6 +31,8 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+
+        viewModel.onCreate()
         PerformanceMeasure.end("LAUNCH")
     }
 }
--- appkoin/src/main/java/me/mattak/dicomparation/MainApplication.kt
+++ appkoin/src/main/java/me/mattak/dicomparation/MainApplication.kt
@@ -1,6 +1,6 @@
 package me.mattak.dicomparation
 
-import android.app.Application
+import androidx.multidex.MultiDexApplication
 import me.mattak.dicomparation.di.repositoryModule
 import me.mattak.dicomparation.di.useCaseModule
 import me.mattak.dicomparation.di.viewModelModule
@@ -8,7 +8,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class MainApplication : Application() {
+class MainApplication : MultiDexApplication() {
     override fun onCreate() {
         PerformanceMeasure.start("LAUNCH")
         super.onCreate()
