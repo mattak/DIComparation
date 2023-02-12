d8270ebefc9792e55af3d7ac1cef38d242b0343c
mattak <mattak.me@gmail.com>
2023-02-12 19:38:49 +0900

    :seedling: create script gen_binding.rs

--- script/clean.sh
+++ script/clean.sh
@@ -6,3 +6,6 @@ cp KoinUseCaseModule.kt appkoin/src/main/java/me/mattak/dicomparation/di/UseCase
 cp KoinRepositoryModule.kt appkoin/src/main/java/me/mattak/dicomparation/di/RepositoryModule.kt
 cp HiltRepositoryModule.kt apphilt/src/main/java/me/mattak/dicomparation/di/RepositoryModule.kt
 cp HiltUseCaseModule.kt apphilt/src/main/java/me/mattak/dicomparation/di/UseCaseModule.kt
+
+find appbinding/src/main/java -type f -name 'Generated*.kt' | xargs rm
+find appbinding/src/main/res/layout -type f -name 'generated_*.xml' | xargs rm
---
+++ script/gen_binding.rs
@@ -0,0 +1,91 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+
+def create_viewholder(name: String, bindingName: String)
+  template = <<-TEMPLATE
+package me.mattak.dicomparation.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import me.mattak.dicomparation.databinding.__BINDING__
+
+class __NAME__(private val binding: __BINDING__) : RecyclerView.ViewHolder(
+    binding.root
+) {
+    companion object {
+        fun create(viewGroup: ViewGroup) = __NAME__(
+            __BINDING__.inflate(
+                LayoutInflater.from(viewGroup.context),
+                viewGroup,
+                false,
+            )
+        )
+    }
+
+    fun bind(cell: TextCell) {
+        binding.value = cell.text
+        binding.executePendingBindings()
+    }
+}
+    TEMPLATE
+
+    return template.gsub(/__NAME__/, name).gsub(/__BINDING__/, bindingName)
+end
+
+def create_layout(name: String) 
+  template = <<-TEMPLATE
+<?xml version="1.0" encoding="utf-8"?>
+<layout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools">
+
+    <data>
+
+        <variable
+            name="value"
+            type="String" />
+    </data>
+
+    <TextView
+        android:id="@+id/text"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:padding="16dp"
+        android:text="@{value}"
+        android:textSize="20sp"
+        android:textStyle="bold"
+        tools:text="@tools:sample/lorem" />
+</layout>
+  TEMPLATE
+  return template
+end
+
+# MAIN
+# ----
+
+if ARGV.size < 1
+  puts "usage: #{$0} <generate_class_count>"
+  exit 0
+end
+
+generateCount = ARGV[0].to_i
+rootname="appbinding"
+viewHolderDirectory = "#{rootname}/src/main/java/me/mattak/dicomparation/ui"
+layoutDirectory = "#{rootname}/src/main/res/layout"
+viewHolders = []
+layouts = []
+
+generateCount.times do |i|
+  viewHolderName = "GeneratedCellText#{i.to_s.rjust(3, '0')}ViewHolder"
+  bindingName = "GeneratedCellText#{i.to_s.rjust(3, '0')}Binding"
+  content = create_viewholder(name: viewHolderName, bindingName: bindingName)
+  File.write("#{viewHolderDirectory}/#{viewHolderName}.kt", content)
+  viewHolders << viewHolderName
+
+  layoutName = "generated_cell_text_#{i.to_s.rjust(3, '0')}"
+  content = create_layout(name: layoutName)
+  File.write("#{layoutDirectory}/#{layoutName}.xml", content)
+  layouts << layoutName
+end
+
